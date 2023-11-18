053f20874ae00aa388b1939b048c44402e9085cd
praj31 <rajputpraj.31@gmail.com>
2023-11-18 16:48:59 -0500

    branch setup

--- crypto/settings.py
+++ crypto/settings.py
@@ -144,5 +144,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # load from .env file
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') # load from .env file
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # load from .env file
 DEFAULT_FROM_EMAIL = "Crypto App <appwydrn@gmail.com>"
