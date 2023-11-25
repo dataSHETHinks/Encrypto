ff106c06093c3d263205069ad1bca745738a6c44
srujanoza <sonuoza24@gmail.com>
2023-11-25 01:34:04 -0500

    Change path after login

--- mainapp/views.py
+++ mainapp/views.py
@@ -30,7 +30,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=raw_password)
             if user is not None:
                 login(request, user)
-                return redirect('')
+                return redirect('/')
         else:
             messages.error(request, "Invalid username or password.", extra_tags='danger')
     else:
